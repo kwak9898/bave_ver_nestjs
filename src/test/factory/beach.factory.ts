6de7ab45893348e1e539f16ad0a72510d1fb11d7
kwak9898 <ktm9837@naver.com>
2023-04-11 18:15:12 +0900

    Add beach factory

---
+++ src/test/factory/beach.factory.ts
@@ -0,0 +1,38 @@
+import { faker } from '@faker-js/faker';
+import { Injectable } from '@nestjs/common';
+import { BeachRepository } from 'src/domains/beach/beach.repository';
+import { Beach } from 'src/domains/beach/entities/beach.entity';
+
+@Injectable()
+export class BeachFactory {
+  constructor(private readonly beachRepository: BeachRepository) {}
+
+  async createBeach(beach?: Beach): Promise<Beach> {
+    beach = new Beach();
+    beach.beachName = faker.name.middleName();
+    beach.sidoName = faker.name.jobArea();
+    beach.gugunName = faker.random.word();
+    beach.latitude = faker.random.alpha();
+    beach.longitude = faker.random.alpha();
+
+    return await this.beachRepository.save(beach);
+  }
+
+  async createBaseBeach(): Promise<Beach> {
+    const beach = new Beach();
+    beach.beachName = faker.name.middleName();
+    beach.sidoName = faker.name.jobArea();
+    beach.gugunName = faker.random.word();
+    beach.latitude = faker.random.alpha();
+    beach.longitude = faker.random.alpha();
+
+    const beachList = [];
+    const saveBeach = await this.beachRepository.save(beach);
+
+    for (let i = 0; i < 10; i++) {
+      beachList.push(await this.createBeach(saveBeach));
+    }
+
+    return saveBeach;
+  }
+}
