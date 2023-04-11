84c464908990181580837bc5d71a421310a095de
kwak9898 <ktm9837@naver.com>
2023-04-11 18:13:43 +0900

    Add beach exception

--- src/exception/errorCode.ts
+++ src/exception/errorCode.ts
@@ -8,3 +8,10 @@ export const USER_EXCEPTION = {
     message: '이미 존재하는 유저입니다.',
   },
 };
+
+export const BEACH_EXCEPTION = {
+  BEACH_NOT_FOUND: {
+    code: 'BEACH_NOT_FOUND',
+    message: '존재하는 해수욕장이 없습니다.',
+  },
+};
