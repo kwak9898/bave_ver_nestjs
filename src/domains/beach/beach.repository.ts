01fb909958758b278b5d9e16d3d7b940fe47c1cd
kwak9898 <ktm9837@naver.com>
2023-05-03 16:31:31 +0900

    FindOneByBeachId

--- src/domains/beach/beach.repository.ts
+++ src/domains/beach/beach.repository.ts
@@ -7,4 +7,13 @@ export class BeachRepository extends Repository<Beach> {
   constructor(private readonly dataSource: DataSource) {
     super(Beach, dataSource.createEntityManager());
   }
+
+  /**
+   * 해수욕장 고유값을 이용한 특정 해수욕장 조회
+   * @param beachId
+   * @returns
+   */
+  async findOneByBeacnId(beachId: number): Promise<Beach> {
+    return this.findOne({ where: { beachId } });
+  }
 }
--- src/domains/beach/beach.service.ts
+++ src/domains/beach/beach.service.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BeachRepository } from './beach.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BEACH_EXCEPTION } from 'src/exception/errorCode';
+import { Beach } from './entities/beach.entity';
 
 @Injectable()
-export class BeachService {}
+export class BeachService {
+  constructor(
+    @InjectRepository(BeachRepository)
+    private readonly beachRepository: BeachRepository,
+  ) {}
+
+  /**
+   * 해수욕장 고유값을 이용한 특정 해수욕장 조회
+   * @param beachId
+   * @returns
+   */
+  async findOneByBeacnId(beachId: number): Promise<Beach> {
+    const beach = await this.beachRepository.findOneByBeacnId(beachId);
+
+    if (!beach) {
+      throw new NotFoundException(BEACH_EXCEPTION.BEACH_NOT_FOUND);
+    }
+
+    return beach;
+  }
+}
