0f5b0521b4599cf7da9bfd7fa60a66096e4126d5
kwak9898 <ktm9837@naver.com>
2023-05-04 14:00:00 +0900

    특정 유저 정보 조회 controller

--- src/domains/beach/beach.controller.ts
+++ src/domains/beach/beach.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { BeachService } from './beach.service';
+import { Beach } from './entities/beach.entity';
 
 @Controller('beach')
-export class BeachController {}
+export class BeachController {
+  constructor(private readonly beachService: BeachService) {}
+
+  @Get('/:beachId')
+  async findOneByBeachId(@Param('beachId') beachId: number): Promise<Beach> {
+    return this.beachService.findOneByBeacnId(beachId);
+  }
+}
